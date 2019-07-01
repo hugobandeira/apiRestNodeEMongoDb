4eac93dcfa4d7081ec8736fcb7721ce5e8806184
HugoBandeira <hugo.bandeira@hotmail.com>
2019-07-01 00:38:51 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 // const bodyParser = require('body-parser');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 app.use(require('./routes'));
+
+
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: false}));
 
 // require('./app/controllers/index')(app);
 
-
-app.listen(3000);
+app.use(cors());
+server.listen(3000);
